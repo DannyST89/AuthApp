d252c0a6fcbcc9feb91a8ff0fd00c82b4085b430
DannyST89 <dsdevelopercr@outlook.com>
2023-05-08 17:57:10 -0600

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using AuthApp.DTOs;
+using AuthApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        /// <summary>
+        /// Calling the User Model
+        /// </summary>
+        public static User user = new User();
+
+        /// <summary>
+        /// Creating asynchronous method that wraps a user retorning a DTO
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>DTO Values to the client</returns>
+        [HttpPost("register")]
+        private async Task<ActionResult<User>> Register(UserDto request) { 
+            
+        }
+    }
+}
---
+++ DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthApp.DTOs
+{
+    public class UserDto
+    {
+        private string UserName { get; set; } = string.Empty;
+        private string Password { get; set; } = string.Empty;
+    }
+}
